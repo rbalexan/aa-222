985d41fb2f87483ac0d5bbfa98cb1687493b3b21
rbalexan <rbalexan@stanford.edu>
2020-04-01 14:28:13 -0500

    update chapter_2 with autodiff

--- chapter_2.jl
+++ chapter_2.jl
@@ -2,6 +2,7 @@ using SymEngine
 using LinearAlgebra # for ⋅
       LinearAlgebra.conj(b::Basic) = b # only valid if real
 using Test
+using ForwardDiff
 
 ## gradient and hessian operations
 create_vars(variable::String, dims::Int) = [symbols("$(variable)$i") for i in 1:dims]
@@ -40,8 +41,8 @@ diff_backward(f, x; h=sqrt(eps(Float64))) = (f(x)     - f(x-h))   / h
 diff_complex( f, x; h=1e-20)              = imag(f(x+h*im))       / h
 
 function complex_step( f, x, h=1e-20)
-      v = f(x+h*im)
-      return (real(v), imag(v)/h)
+    v = f(x+h*im)
+    return (real(v), imag(v)/h)
 end
 
 # function i
@@ -52,3 +53,40 @@ didx = x -> 2*x*cos(x^2)
 
 @test i(   π/2) ≈ value
 @test didx(π/2) ≈ deriv
+
+## automatic differentiation using forward accumulation
+struct Dual
+    v
+    ∂
+end
+
+# overload Base functions
+Base.:+( a::Dual, b::Dual) = Dual(a.v + b.v, a.∂ + b.∂)
+Base.:*( a::Dual, b::Dual) = Dual(a.v * b.v, a.v*b.∂ + b.v*a.∂)
+Base.log(a::Dual)          = Dual(log(a.v), a.∂/a.v)
+function Base.max(a::Dual, b::Dual)
+    v = max(a.v, b.v)
+    ∂ = a.v > b.v ? a.∂ : a.v < b.v ? b.∂ : NaN # NaN if equal
+    return Dual(v, ∂)
+end
+function Base.max(a::Dual, b::Int)
+    v = max(a.v, b)
+    ∂ = a.v > b ? a.∂ : a.v < b ? 0 : NaN # NaN if equal
+    return Dual(v, ∂)
+end
+
+a = Dual(3, 1)
+b = Dual(2, 0)
+
+log(a*b + max(a,2))
+
+# using ForwardDiff Dual type
+a = ForwardDiff.Dual(3,1)
+b = ForwardDiff.Dual(2,0)
+
+log(a*b + max(a,2))
+
+## automatic differentiation using reverse accumulation
+import Zygote: gradient
+f(a, b) = log(a*b + max(a,2))
+gradient(f, 3.0, 2.0)
